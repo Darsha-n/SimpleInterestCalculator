645122b6a88174e25fd5479abbaa54f9eb61b40a
darsha-n <darshanganiger55@gmail.com>
2025-06-09 12:06:32 +0530

    save file

---
+++ SimpleInterestCalculator.java
@@ -0,0 +1,18 @@
+public class SimpleInterestCalculator{
+
+    public static double CalculateSimpleInterest(double principal , double rate , double time) {
+        return(principal*rate*time)/100;
+    }
+    public static void main(String[]args){
+
+        double principal = 10000;
+        double rate = 4;
+        double time = 2;
+
+        double interest = CalculateSimpleInterest(principal, rate, time);
+        System.out.println("Principal:"+ principal);
+        System.out.println("Rate:"+ rate);
+        System.out.println("Time:"+ time);
+        System.out.println("SimpleInterest:"+ interest);
+    }
+}
